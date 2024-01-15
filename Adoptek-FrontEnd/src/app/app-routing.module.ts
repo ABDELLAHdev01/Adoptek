85ea4bf646bf987ed0be65c64db9927602d20868
ABDELLAHdev01 <abdellahelghoulamdev@gmail.com>
2024-01-15 09:36:27 +0100

    Front-End: generating LoginForm

--- Adoptek-FrontEnd/src/app/app-routing.module.ts
+++ Adoptek-FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './layout/auth/auth.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginFormComponent
+      },
+     
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Adoptek-FrontEnd/src/app/app.module.ts
+++ Adoptek-FrontEnd/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './layout/auth/auth.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
