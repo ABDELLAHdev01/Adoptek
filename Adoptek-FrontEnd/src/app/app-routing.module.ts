9d792c6b948c5657c224d429bf6ea20929e4be82
ABDELLAHdev01 <abdellahelghoulamdev@gmail.com>
2024-01-15 11:11:58 +0100

    front-end : generate navbar and style it

--- Adoptek-FrontEnd/src/app/app-routing.module.ts
+++ Adoptek-FrontEnd/src/app/app-routing.module.ts
@@ -2,17 +2,33 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './layout/auth/auth.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { RegisterFormComponent } from './components/register-form/register-form.component';
+import { HomePageComponent } from './layout/home-page/home-page.component';
 
 const routes: Routes = [
   {
-    path: 'auth',
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path: "auth",
     component: AuthComponent,
     children: [
       {
-        path: 'login',
+        path: "login",
         component: LoginFormComponent
-      },
-     
+      }
+      ,
+      {
+        path: "register",
+        component: RegisterFormComponent
+      }
+      ,
+      {
+        path: "**",
+        redirectTo: "login"
+
+      }
     ]
   }
 ];
--- Adoptek-FrontEnd/src/app/app.module.ts
+++ Adoptek-FrontEnd/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './layout/auth/auth.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { RegisterFormComponent } from './components/register-form/register-form.component';
+import { HomePageComponent } from './layout/home-page/home-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    RegisterFormComponent,
+    HomePageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
