538a64684b33116a967214da9af4087795872040
ABDELLAHdev01 <abdellahelghoulamdev@gmail.com>
2024-01-18 18:04:15 +0100

    Backend: code CorsConfig to allow connection between api and angular

---
+++ Adoptek-BackEnd/AdoptekSpring/src/main/java/com/example/adoptekspring/config/CorsConfig.java
@@ -0,0 +1,16 @@
+package com.example.adoptekspring.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+//    corsconfig for angular
+    @Override
+    public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200") // Angular app's URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+}
