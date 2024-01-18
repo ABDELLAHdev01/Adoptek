f6b711210c4d0fb404ac529b0a001a7190413d8d
ABDELLAHdev01 <abdellahelghoulamdev@gmail.com>
2024-01-18 17:39:49 +0100

    Backend: Code article and tags entite

--- Adoptek-BackEnd/AdoptekSpring/src/main/java/com/example/adoptekspring/domain/Tag.java
+++ Adoptek-BackEnd/AdoptekSpring/src/main/java/com/example/adoptekspring/domain/Tag.java
@@ -1,4 +1,20 @@
 package com.example.adoptekspring.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
 }
