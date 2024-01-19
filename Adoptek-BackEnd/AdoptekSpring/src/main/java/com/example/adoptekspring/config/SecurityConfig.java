9c27c22bf200a99608c6aa3be312144b41f203d5
ABDELLAHdev01 <abdellahelghoulamdev@gmail.com>
2024-01-19 11:32:22 +0100

    Backend: make user implement Userdetails and override all methods

---
+++ Adoptek-BackEnd/AdoptekSpring/src/main/java/com/example/adoptekspring/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.example.adoptekspring.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends  {
+
+}
--- Adoptek-BackEnd/AdoptekSpring/src/main/java/com/example/adoptekspring/domain/User.java
+++ Adoptek-BackEnd/AdoptekSpring/src/main/java/com/example/adoptekspring/domain/User.java
@@ -3,7 +3,11 @@ package com.example.adoptekspring.domain;
 import com.example.adoptekspring.domain.enm.Role;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -13,7 +17,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -34,4 +38,38 @@ public class User {
     @OneToMany
     private List<Pet> pets;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
