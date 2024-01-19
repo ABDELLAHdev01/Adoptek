7410dd181e38816c0a4ec419471bbefb714e3162
ABDELLAHdev01 <abdellahelghoulamdev@gmail.com>
2024-01-19 11:22:54 +0100

    Backend: make crud of tags

--- Adoptek-BackEnd/AdoptekSpring/src/main/java/com/example/adoptekspring/service/TagService.java
+++ Adoptek-BackEnd/AdoptekSpring/src/main/java/com/example/adoptekspring/service/TagService.java
@@ -1,4 +1,18 @@
 package com.example.adoptekspring.service;
 
+import com.example.adoptekspring.domain.Tag;
+
+import java.util.List;
+
 public interface TagService {
+
+    Tag createTag(Tag tag);
+
+    Tag updateTag(Tag tag);
+
+    void deleteTag(Long id);
+
+    Tag getTagById(Long id);
+
+    List<Tag> getAllTags();
 }
--- Adoptek-BackEnd/AdoptekSpring/src/main/java/com/example/adoptekspring/service/implementation/TagServiceImpl.java
+++ Adoptek-BackEnd/AdoptekSpring/src/main/java/com/example/adoptekspring/service/implementation/TagServiceImpl.java
@@ -1,8 +1,51 @@
 package com.example.adoptekspring.service.implementation;
 
+import com.example.adoptekspring.domain.Tag;
+import com.example.adoptekspring.repository.TagRepository;
 import com.example.adoptekspring.service.TagService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TagServiceImpl implements TagService {
+
+    private final TagRepository tagRepository;
+
+    public TagServiceImpl(TagRepository tagRepository) {
+        this.tagRepository = tagRepository;
+    }
+
+    @Override
+    public Tag createTag(Tag tag) {
+        return tagRepository.save(tag);
+    }
+
+    @Override
+    public Tag updateTag(Tag tag) {
+        Tag tag2 = tagRepository.findById(tag.getId()).orElseThrow(() -> new RuntimeException("Tag not found"));
+        tag2.setName(tag.getName());
+        return tagRepository.save(tag2);
+    }
+
+    @Override
+    public void deleteTag(Long id) {
+        tagRepository.deleteById(id);
+    }
+
+    @Override
+    public Tag getTagById(Long id) {
+        Tag tag = tagRepository.findById(id).orElseThrow(() -> new RuntimeException("Tag not found"));
+        return tag;
+
+    }
+
+    @Override
+    public List<Tag> getAllTags() {
+        List<Tag> tags = tagRepository.findAll();
+        if (tags.isEmpty()) {
+            throw new RuntimeException("No tags found");
+        }
+        return tags;
+    }
 }
