855aed34fe2842c7a680a6ed1e5d422290643324
ABDELLAHdev01 <abdellahelghoulamdev@gmail.com>
2024-02-10 18:20:15 +0100

    Frontend: Adding User Entity and UserState

---
+++ Adoptek-FrontEnd/src/app/domain/user-entity.ts
@@ -0,0 +1,16 @@
+export interface UserEntity {
+    firstName: string;
+    lastName: string;
+    email: string;
+    phoneNumber: string;
+    address: string;
+    image: string;
+    city: string;
+    country: string;
+    state: string;
+    zipCode: string;
+    status: string;
+    password: string;
+    token?: string;
+    
+}
---
+++ Adoptek-FrontEnd/src/app/state/user/user-state.ts
@@ -0,0 +1,8 @@
+import { UserEntity } from "src/app/domain/user-entity";
+
+export interface UserState {
+    User: UserEntity;
+    IsAuthenticated: boolean;
+    IsLoading: boolean;
+    ErrorMessage: string | null;
+}
