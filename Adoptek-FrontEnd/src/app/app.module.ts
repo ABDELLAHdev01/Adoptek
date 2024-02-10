181581ce24f45056343bb4fc0ae2f20fe7dbcbff
ABDELLAHdev01 <abdellahelghoulamdev@gmail.com>
2024-02-10 18:09:44 +0100

    Frontend: installing redux devtool

--- Adoptek-FrontEnd/src/app/app.module.ts
+++ Adoptek-FrontEnd/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -18,6 +18,7 @@ import { StatisticsComponent } from './components/statistics/statistics.componen
 import { ArticelsCardsComponent } from './components/articels-cards/articels-cards.component';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -40,7 +41,8 @@ import { EffectsModule } from '@ngrx/effects';
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot({}, {}),
-    EffectsModule.forRoot([])
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
   ],
   providers: [],
   bootstrap: [AppComponent]
